d6507842a2e74407d32cb13eb89cbb7bd0541f93
MrJack <bhatsalman221@gmail.com>
2025-11-03 00:58:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -93,4 +93,8 @@ def removeimg():
       print("error")
     return render_template("bg-remove.html", message="")
  
+import os
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
